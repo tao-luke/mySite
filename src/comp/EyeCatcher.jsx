1c9ac41af259625d6d55bb1c9a66fd52f2813e25
luke <luketao@icloud.com>
2021-01-10 01:14:04 -0500

    set up profile image and resume link

--- src/comp/EyeCatcher.jsx
+++ src/comp/EyeCatcher.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import '../scss/eyeCatcher.scss';
+import avatar from '../resources/image/profile.png';
 class EyeCatcher extends Component {
 	state = {};
 	render() {
 		return (
 			<div className="eyeCatchContainer">
-				<div className="blurred-box" />
+				<div className="blurred-box">
+					<div className="profile">
+						<img src={avatar} alt="Luke's Profile :)" />
+						<button className="btn btn-outline-success"> Resume</button>
+					</div>
+					<div className="eyecatch" />
+				</div>
 			</div>
 		);
 	}
