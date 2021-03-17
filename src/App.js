2c71e19926e121f1b90cef9186bb1861656333dd
luke <luketao@icloud.com>
2021-03-17 14:17:27 -0400

    attempt to optimize meta tags

--- src/App.js
+++ src/App.js
@@ -7,16 +7,6 @@ function App() {
 		<React.Fragment>
 			<Helmet>
 				<title>Luke Tao </title>
-
-				<meta charset="UTF-8" />
-				<meta
-					name="description"
-					content="The main page of Luke Tao, a computer science student at University of Waterloo"
-				/>
-				<meta name="author" content="Luke Tao" />
-				<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1" />
-
-				<meta name="keywords" content="Luke Tao LukeResume Resume Luke LukeResume" />
 			</Helmet>
 			<Page />
 		</React.Fragment>
